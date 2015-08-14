016f7bc58213dac813c3b208f749380134ef1d55
kennyykw <kennyyjw@gmail.com>
2015-08-14 09:59:59 -0700

    Basic tip calculator layout and functionality.

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var tipLabel: UILabel!
 
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func inEditingChanged(sender: AnyObject) {
+        
+        var tipPercentages = [0.18, 0.2, 0.25]
+        var tipPercentage = tipPercentages [tipControl.selectedSegmentIndex]
+        
+        
+        var billAmount = NSString(string: billTextField.text).doubleValue
+        var tips = billAmount * tipPercentage
+        var total = billAmount + tips
+        
+        tipLabel.text = "$\(tips)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tips)
+        totalLabel.text = String(format: "$%.2f", total)
+        
+        tipControl.alpha = 0
+        
+        UISegmentedControl.animateWithDuration(0.4, animations: {
+            self.tipControl.alpha = 1
+        })
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
 }
 
