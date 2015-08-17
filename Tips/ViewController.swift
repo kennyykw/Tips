c9daaa0246918f88ed1e752c6db3974ac2e23054
kennyykw <kennyyjw@gmail.com>
2015-08-16 17:02:11 -0700

    Added image assets Added animations

--- Tips/ViewController.swift
+++ Tips/ViewController.swift
@@ -8,22 +8,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
     
     @IBOutlet weak var tipLabel: UILabel!
 
     @IBOutlet weak var totalLabel: UILabel!
     
-    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var billTextField: UITextField! {
+        didSet {
+            billTextField.delegate = self
+        }
+    }
     
     @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    @IBOutlet weak var tipStaticText: UILabel!
+    
+    @IBOutlet weak var totalStaticLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        tipControl.alpha = 0
+        tipLabel.alpha = 0
+        tipStaticText.alpha = 0
+        totalStaticLabel.alpha = 0
+        totalLabel.alpha = 0
         
-        
+        billTextField.attributedPlaceholder = NSAttributedString (string:"$Bill Amount", attributes:[NSForegroundColorAttributeName: UIColor.whiteColor()])
     }
 
     override func didReceiveMemoryWarning() {
@@ -47,17 +61,42 @@ class ViewController: UIViewController {
         tipLabel.text = String(format: "$%.2f", tips)
         totalLabel.text = String(format: "$%.2f", total)
         
-        tipControl.alpha = 0
-        
-        UISegmentedControl.animateWithDuration(0.4, animations: {
-            self.tipControl.alpha = 1
-        })
-        
     }
 
     @IBAction func onTap(sender: AnyObject) {
         
         view.endEditing(true)
     }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+
+        self.tipControl.center = CGPoint (x: 0, y: 1000)
+        self.tipLabel.center = CGPoint (x: 260, y: 1257)
+        self.tipStaticText.center = CGPoint (x: 0, y: 1000)
+        self.totalStaticLabel.center = CGPoint(x: 0, y: 1000)
+        self.totalLabel.center = CGPoint(x: 260, y: 1317)
+        
+        UIView.animateWithDuration(0.4, animations: { () -> Void in
+            self.tipControl.alpha = 1
+            self.tipControl.center = CGPoint(x: 0, y: 0)
+        
+            self.tipLabel.alpha = 1
+            self.tipLabel.center = CGPoint (x: 260, y: 257)
+            
+            self.tipStaticText.alpha = 1
+            self.tipStaticText.center = CGPoint(x: 0, y: 0)
+            
+            self.totalStaticLabel.alpha = 1
+            self.totalStaticLabel.center = CGPoint(x: 0, y: 0)
+            
+            self.totalLabel.alpha = 1
+            self.totalLabel.center = CGPoint(x: 260, y: 317)
+                    
+            
+        })
+        return true
+    }
+    
+    
 }
 
